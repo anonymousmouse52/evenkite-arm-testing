cd9f9229edb5ee368f95bb31c31833b43bbfd8a6
Emi <120683059+emberaine@users.noreply.github.com>
2026-07-13 21:38:17 -0700

    cleanup somewhat

---
+++ files/scripts/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rm -f /usr/share/applications/nvim.desktop
+rm -f /usr/share/applications/btop.desktop
+rm -f /usr/share/applications/nvtop.desktop
+rm -f /usr/share/applications/htop.desktop
