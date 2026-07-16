baccf61b05ca9b343dd3f01fc47a6f310fe1cd3a
Emi <120683059+emberaine@users.noreply.github.com>
2026-07-16 15:46:42 -0700

    More user choice, like using bash, still need to fix starship thingy

--- files/system/etc/profile.d/bash-to-fish.sh
+++ files/system/etc/profile.d/bash-to-fish.sh
@@ -4,7 +4,10 @@
 # - 'fish' is not the parent process of this shell
 # - We did not call: bash -c '...'
 # - The fish binary exists and is executable
-if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} && -x "/bin/fish" ]]; then
-  shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
-  exec fish $LOGIN_OPTION
+# - User does not have .no-bash-to-fish in fish config dir
+if [ ! -f "$HOME/.config/fish/.no-bash-to-fish" ]; then
+    if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} && -x "/bin/fish" ]]; then
+    shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
+    exec fish $LOGIN_OPTION
+    fi
 fi
