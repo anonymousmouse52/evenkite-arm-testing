88599f9b6acdbebad488da7535a289978c6f8352
Emi <120683059+emberaine@users.noreply.github.com>
2026-07-16 21:09:18 -0700

    tons of backend changes and replacing starship with oh-my-posh

--- files/system/etc/fish/config.fish
+++
@@ -1,4 +0,0 @@
-set fish_greeting
-
-set -gx EDITOR nvim
-set -gx VISUAL nvim
--- files/system/etc/profile.d/evenkite-bash-to-fish.sh
+++ files/system/etc/profile.d/evenkite-bash-to-fish.sh
@@ -1,12 +1,18 @@
+# PLEASE DO NOT TOUCH THIS FILE!
+
+
+# .-*   Evenkite Helper Script   *-.
+
 # Automatically use fish instead of bash, safely and properly
 
 # Only trigger if:
 # - 'fish' is not the parent process of this shell
 # - We did not call: bash -c '...'
 # - The fish binary exists and is executable
+# - The shell is interactive
 # - User does not have .no-bash-to-fish in fish config dir
 if [ ! -f "$HOME/.config/fish/.no-bash-to-fish" ]; then
-    if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} && -x "/bin/fish" ]]; then
+    if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} && -x "/bin/fish" && -z "$PS1" ]]; then
     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
     exec fish $LOGIN_OPTION
     fi
