eda4f38161e8a5d30d44ec584fda9594c30b45b6
Emi <120683059+emberaine@users.noreply.github.com>
2026-07-17 18:33:26 -0700

    it should work now fr

--- files/scripts/finishingtouches.sh
+++ files/scripts/finishingtouches.sh
@@ -7,4 +7,6 @@ rm -f /usr/share/applications/btop.desktop
 rm -f /usr/share/applications/nvtop.desktop
 rm -f /usr/share/applications/htop.desktop
 
+echo "import \"/usr/share/bluebuild/justfiles/00-default.just\"" >> test.txt
+
 dconf update
